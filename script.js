391dafc1decad852419b9050e65be9f737a93ce0
mwynott <mwynott1989@gmail.com>
2026-06-02 19:28:08 -0400

    Finish basic concept of gameboard IIFE

--- script.js
+++ script.js
@@ -12,8 +12,16 @@ const player = (name, marker) => {
 const gameBoard = (() => {
     //Array to hold empty spaces on the board
     const board = Array(9).fill('');
+    const getBoard = () => board;
 
-
-
-
+    //Function to update the board with the player's marker
+    const updateBoard = (index, marker) => {
+        if (board[index] === '') {
+            board[index] = marker;
+            return true;
+        }
+        return false;
+    };
+    return {getBoard, updateBoard};
+})();
 //Game Controller
