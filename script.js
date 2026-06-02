327538760b480b596bab845b35db42af33cf6c50
mwynott <mwynott1989@gmail.com>
2026-06-02 19:56:57 -0400

    Add part of IIFE for game controller

--- script.js
+++ script.js
@@ -24,4 +24,29 @@ const gameBoard = (() => {
     };
     return {getBoard, updateBoard};
 })();
+
 //Game Controller
+const gameController = (() => {
+    let currentPlayer;
+    const player1 = player('Player 1', 'X');
+    const player2 = player('Player 2', 'O');
+
+//Function to switch the current player
+    const switchPlayer = () => {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    };
+//Function to check for a win condition
+    const checkWin = () => {
+        const board = gameBoard.getBoard();
+        //All possible win conditions for indices on 3x3 grid
+        const winConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+    }
